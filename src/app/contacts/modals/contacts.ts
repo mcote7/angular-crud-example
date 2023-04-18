fef10bf909ee8e6c37aa5a2150e9874e2da00730
Michael Cote <michaelcote7@outlook.com>
2023-04-18 16:29:55 -0700

    contacts service complete

--- src/app/contacts/modals/contacts.ts
+++ src/app/contacts/modals/contacts.ts
@@ -1,5 +1,6 @@
 export interface Contact {
-    id: number;
+    property: boolean;
+    id?: number;
     name: string;
     email: string;
     phone: string;
--- src/app/contacts/services/contacts.service.ts
+++ src/app/contacts/services/contacts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Contact } from '../modals/contacts';
 
 
@@ -9,6 +9,8 @@ import { Contact } from '../modals/contacts';
 })
 export class ContactsService {
 
+    private readonly API_URL = "contacts_list";
+
     private contacts = new BehaviorSubject<Array<Contact>>([]);
     public contacts$ = this.contacts.asObservable();
 
@@ -16,12 +18,39 @@ export class ContactsService {
         this.getContacts().subscribe();
     }
 
-    private getContacts() {
-        return this.http.get<Array<Contact>>('contacts_list')
+    // TODO: add loading state ... 
+    private getContacts(): Observable<Array<Contact>> {
+        return this.http.get<Array<Contact>>(this.API_URL)
             .pipe(
                 tap((contacts) => this.contacts.next(contacts))
             );
     }
 
-    // post, patch, delete ... 
+    public postContact(contact: Contact): Observable<Contact> {
+        return this.http.post<Contact>(this.API_URL, contact)
+            .pipe(
+                tap((newContact) => this.contacts.next([...this.contacts.getValue(), newContact]))
+            );
+    }
+
+    public deleteContact(contactId: number): Observable<Object> {
+        return this.http.delete<Contact>(`${this.API_URL}/${contactId}`)
+            .pipe(
+                tap((_) => this.contacts.next([...this.contacts.getValue().filter((contact) => contact.id !== contactId)]))
+            );
+    }
+
+    public favoriteContact(contact: Contact): Observable<Contact> {
+        return this.http.patch<Contact>(`${this.API_URL}/${contact.id}`, { favorite: !contact.favorite })
+            .pipe(
+                tap((updatedContact) => contact.favorite = updatedContact.favorite)
+            );
+    }
+
+    public editContact(contact: Contact, editedContact: Contact): Observable<Contact> {
+        return this.http.put<Contact>(`${this.API_URL}/${contact.id}`, editedContact)
+            .pipe(
+                tap((updatedContact) => contact = updatedContact)
+            );
+    }
 }
