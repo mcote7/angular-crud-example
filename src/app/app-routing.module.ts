f15e398bcbc91040f765be8270e74abefa1fb783
Michael Cote <michaelcote7@outlook.com>
2023-04-17 17:19:14 -0700

    contacts module with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'example-1', pathMatch: 'full' },
-    // { path: '**', component: NotFoundComponent }
+    { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
