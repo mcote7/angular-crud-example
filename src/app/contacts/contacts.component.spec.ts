1a116d2dbbb3e9f49c0eb086daa7a52f8649596f
Michael Cote <michaelcote7@outlook.com>
2023-07-13 11:38:32 -0700

    ...

--- src/app/contacts/contacts.component.spec.ts
+++ src/app/contacts/contacts.component.spec.ts
@@ -3,27 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContactsComponent } from './contacts.component';
 import { ContactTableComponent } from './components/contact-table/contact-table.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 
 describe('ContactsComponent', () => {
-  let component: ContactsComponent;
-  let fixture: ComponentFixture<ContactsComponent>;
+    let component: ContactsComponent;
+    let fixture: ComponentFixture<ContactsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-        imports: [HttpClientModule, MatDialogModule, BrowserAnimationsModule, MatTableModule],
-      declarations: [ ContactsComponent, ContactTableComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientModule, MatDialogModule, BrowserAnimationsModule, MatTableModule],
+            declarations: [ContactsComponent, ContactTableComponent]
+        }).compileComponents();
+        fixture = TestBed.createComponent(ContactsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-    fixture = TestBed.createComponent(ContactsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
